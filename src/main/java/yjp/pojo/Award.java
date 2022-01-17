ac5d24340c9c3be501b4e93160c2a1dcce360738
yangjingping <yangjingping@bytedance.com>
2022-01-17 15:02:00 +0800

    fix:修改连接的数据库为city

---
+++ src/main/java/yjp/pojo/Award.java
@@ -0,0 +1,12 @@
+package yjp.pojo;
+
+public class Award {
+    private Integer id;
+    private String name;
+    private String type;
+    private String grade;
+    private Integer cont_id;
+    private Integer org_id;
+    private Integer user_id;
+    private Integer team_id;
+}
